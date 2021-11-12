e5a3f49f2498dee9f566baca59b690aacd958309
Talha Qureshi <Talha215@users.noreply.github.com>
2021-11-12 13:46:12 -0500

    Update note.java

--- BumblebeeJava/src/note.java
+++ BumblebeeJava/src/note.java
@@ -1,6 +1,6 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
 
 public class note extends commandClass implements commandInterface {
     ArrayList<String> commandPhrases = new ArrayList<String>();
@@ -16,22 +16,29 @@ public class note extends commandClass implements commandInterface {
     public String run(String[] input){
         String fileName = "";
         int i, j, k, l=0;
-        for(i=0; i<input.size(); i++){
-            if(input.get(i).equals("note")){
+        for(i=0; i<input.length; i++){
+            if(input[i].equals("note")){
                 l = i;
                 break;
             }
         }
         for(j=0; j<l; j++){
             if(j == i-1)
-                fileName = fileName.concat(input.get(j));
+                fileName = fileName.concat(input[j]);
             else
-                fileName = fileName.concat(input.get(j) + " ");
+                fileName = fileName.concat(input[j] + " ");
         }
-        FileWriter writer = new FileWriter(fileName + ".txt");
-        for(k=(i+1); k<input.size(); k++){
-            writer.write(input.get(k) + " ");
-        }
-        writer.close();
+        try {
+        	FileWriter writer = new FileWriter(fileName + ".txt");
+            for(k=(i+1); k<input.length; k++){
+                writer.write(input[k] + " ");
+            }
+            writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return "Made note: " + fileName;
     }
 }
