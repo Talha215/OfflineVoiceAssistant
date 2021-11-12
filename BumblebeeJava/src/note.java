b826f041b71e9111fa8f132316ee2aa895b2e491
ksnyder23 <92048888+ksnyder23@users.noreply.github.com>
2021-11-12 13:33:39 -0500

    Update note.java

--- BumblebeeJava/src/note.java
+++ BumblebeeJava/src/note.java
@@ -12,19 +12,23 @@ public class note extends commandClass implements commandInterface {
 
     public String run(String[] input){
         String fileName = "";
-        for(i=0;i++;i<input.length){
-            if(input[i].equals(note)){
-                for(j=0;j++;j<i){
-                    if(i != j)
-                        fileName = fileName.concat(input[i] + " ");
-                    else
-                        fileName = fileName.concat(input[i]);
-                }
+        int i, j, k, l=0;
+        for(i=0; i<input.size(); i++){
+            if(input.get(i).equals("note")){
+                l = i;
+                break;
             }
         }
-        FileWriter writer = new FileWriter(fileName + ".txt");
-        for(k=(i+1);k++;k<input.length-1){
-            writer.write(input[k] + System.lineSeparator());
+        for(j=0; j<l; j++){
+            if(j == i-1)
+                fileName = fileName.concat(input.get(j));
+            else
+                fileName = fileName.concat(input.get(j) + " ");
         }
+        FileWriter writer = new FileWriter(fileName + ".txt");
+        for(k=(i+1); k<input.size(); k++){
+            writer.write(input.get(k) + " ");
+        }
+        writer.close();
     }
 }
