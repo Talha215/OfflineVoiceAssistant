5091dbc02841cb088411ff25534e44dd6fc2f743
ijg1996 <92048967+ijg1996@users.noreply.github.com>
2021-11-16 18:51:48 -0500

    Add files via upload
    Test versions of calculator classes.
    calc does dijkstras 2 stack
    calculator is the command class
    StringToMath converts string to integers for math.

---
+++ BumblebeeJava/src/calc.java
@@ -0,0 +1,87 @@
+/**
+
+ * 
+ */
+
+
+import java.util.Stack;
+
+/**
+ * @author Ian Zichko-Geithner
+ *
+ */
+public class calc {
+	static String input;
+	static String outPut;
+	/**
+	 * Constructor
+	 */
+	calc(){
+		this.input = "";
+		this.outPut = " ";
+	}
+	
+	/**
+	 * dijkstra's two stack algorithm used to convert string and calculate then return string
+	 * @param in
+	 * @return outPut 
+	 *
+	 */
+	public static String calculate(String in) {
+		 input=in;
+         String exp[] = input.split(" ");
+         Stack<String> ops = new Stack<String>();
+         Stack<Double> vals = new Stack<Double>();
+
+         for(int i = exp.length-1; i >=0 ; i--) {
+        	 	 //iterates through string and checks each token in the string math expression
+        	 	 //Checks for operators to push into operator stack, and values to push into value stack.
+                 String current = exp[i];
+                 if (current.equals(")") || current.equals("")) {
+                 }
+                 else if (current.equals("+") || current.equals("*") || current.equals("-") || current.equals("^") || current.equals("/")) {
+                	 ops.push(current);
+                 } 
+                 else if (current.equals("(")) {
+                	 getComp(ops, vals);
+                 } 
+                 else {
+                	 vals.push(Double.parseDouble(current));
+	    }
+         }
+         //iterates to calc each operation given
+         for(int i =ops.size()-1;i>=0;i--) {
+        	 getComp(ops, vals);
+         }
+         outPut = vals.pop().toString();
+		return outPut;
+	}
+	
+	/**
+	 * Pops the operation then uses it to calculate the 2 top values in the value stack
+	 * @param in
+	 * @return outPut 
+	 *
+	 */
+	private static void getComp(Stack<String> ops, Stack<Double> vals) {
+		String op = ops.pop();
+		if (op.equals("+")) {
+			vals.push(vals.pop() + vals.pop());
+		} 
+		else if (op.equals("*")) {
+			vals.push(vals.pop() * vals.pop());
+		} 
+		else if (op.equals("-")) {
+			vals.push(vals.pop() - vals.pop());
+		}
+		else if (op.equals("^")) {
+			vals.push(Math.pow(vals.pop(), vals.pop()));
+		}
+		else if (op.equals("/")) {
+			vals.push(vals.pop()/vals.pop());
+		}
+		
+	}
+	
+
+ }
---
+++ BumblebeeJava/src/calculator.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author No This is Patrick
+ *
+ */
+public class calculator extends commandClass implements commandInterface {
+	ArrayList<String> commandPhrases = new ArrayList<String>();
+	StringToMath converter = new StringToMath();
+	calc stack = new calc();
+	
+	public calculator() {
+		commandPhrases.add("plus");
+		commandPhrases.add("minus");
+		commandPhrases.add("times");
+		commandPhrases.add("divide");
+		commandPhrases.add("divided");
+		commandPhrases.add("multiplied");
+		commandPhrases.add("multiply");
+	}
+
+	@Override
+	public boolean match(String token) {
+		return commandPhrases.contains(token);
+	}
+
+	@Override
+	public String run(String[] command) {
+		String fullCommand = "";
+		for(int i=0;i<command.length;i++) {
+			fullCommand += command[i] + " ";
+		}
+		String intMath = converter.convert(fullCommand);
+		String output = stack.calculate(intMath);
+		System.out.println(intMath);
+		System.out.println(output);
+		return output;
+	}
+
+}
