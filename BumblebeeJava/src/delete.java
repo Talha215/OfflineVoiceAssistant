f73d2e0c455689538f71f4b391f692de50c5eb52
kathleen <kathleensnyder23@gmail.com>
2021-11-30 11:58:57 -0500

    more modules stuff

---
+++ BumblebeeJava/src/delete.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class date extends commandClass implements commandInterface {
+    ArrayList<String> commandPhrases = new ArrayList<String>();
+
+    public date() {
+        commandPhrases.add("delete");
+    }
+
+    public boolean match(String word){
+        return commandPhrases.contains(word);
+    }
+
+    public String run(String[] input){
+        String filename "";
+        for(j=1; j<input.length; j++){
+            if(j == input.length)
+                fileName = fileName.concat(input[j]);
+            else
+                fileName = fileName.concat(input[j] + " ");
+        }
+        File file = new File(filename + ".txt");
+        if (file.delete()) {
+            return ("successfully deleted " + filename);
+        } else {
+            return ("no file called " + filename + " could be deleted");
+    }
+}
