96c3af860bed0581917004c2799d6b004a7e80fd
ijg1996 <92048967+ijg1996@users.noreply.github.com>
2021-12-03 15:44:56 -0500

    Added Timer and GUI
    BEEP BEEP BEEP

---
+++ BumblebeeJava/src/GUI.java
@@ -0,0 +1,113 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+
+public class GUI {
+
+	private JFrame frame;
+	private static JTextField outputField;
+	private static JTextField inputField;
+
+	/**
+	 * Launch the application.
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		while(true) {	
+			Scanner input = new Scanner(System.in);
+			String output = null;
+
+			do {
+				try {
+					input = new Scanner(new BufferedReader(new FileReader("test.txt")));
+					
+					output = "";
+					while(input.hasNext()) {
+						output += input.nextLine() + '\n';
+					}
+					input.close();
+					File toDelete = new File("test.txt");
+					toDelete.delete();
+					
+					//string manipulation of output	
+					output = output.substring(14, output.lastIndexOf('\"'));
+					System.out.println("input: " + output);
+					
+				} catch(FileNotFoundException e ) {
+					Thread.sleep(1000);
+				}
+			} while(output == null);
+			
+			stateMachine run;
+			try {
+				run = new stateMachine(output);
+				String speech = run.outPut(run.match());
+				outputField.setText(speech);
+				inputField.setText(output);
+				stateMachine.textToSpeech.setVoice("dfki-poppy-hsmm");
+				stateMachine.textToSpeech.speak(speech, 1.5f, false, true);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.ORANGE);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel = new JLabel("Input:");
+		frame.getContentPane().add(lblNewLabel);
+		
+		inputField = new JTextField();
+		inputField.setEditable(false);
+		frame.getContentPane().add(inputField);
+		inputField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Output:");
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		outputField = new JTextField();
+		outputField.setEditable(false);
+		frame.getContentPane().add(outputField);
+		outputField.setColumns(10);
+	}
+
+}
---
+++ BumblebeeJava/src/timer.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author Ian Zichko-Geithner
+ *
+ */
+public class timer extends commandClass implements commandInterface {
+	ArrayList<String> commandPhrases = new ArrayList<String>();
+	StringToMath stringToMath = new StringToMath();
+	static TextToSpeech timerTextToSpeech = new TextToSpeech();
+	calc calc = new calc();
+	Timer timer;
+
+	public timer() {
+		commandPhrases.add("timer");
+	}
+
+	@Override
+	public boolean match(String token) {
+		return commandPhrases.contains(token);
+	}
+
+	@Override
+	public String run(String[] command) {
+		String fullCommand = "";
+		String returnTime = "";
+		for(int i=0;i<command.length;i++) {
+			if(command[i].equals("for")) {
+				
+			}
+			else if(command[i].equals("and")) {
+				fullCommand+="plus ";
+			}
+			else if(command[i].equals("minutes") || command[i].equals("minute")) {
+				fullCommand+="times sixty ";
+				if(i>=1) {
+					returnTime+= command[i-1] + command[i] + " ";
+				}
+				else {
+					returnTime+= command[i-1] + command[i] + " ";
+				}
+			}
+			else if(command[i].equals("hour") || command[i].equals("hours")) {
+				if(i>0 && command[i-1].equals("an")) {
+					fullCommand+= "one times sixty times sixty";
+				}
+				else {
+					fullCommand+="times sixty times sixty ";
+				}
+				if(i>=1) {
+					returnTime+= command[i-1] + command[i] + " ";
+				}
+				else {
+					returnTime+= command[i-1] + command[i] + " ";
+				}
+			}
+			else if(command[i].equals("seconds") || command[i].equals("second")) {
+				fullCommand+=" ";
+				if(i>=1) {
+					returnTime+= command[i-1] + command[i] + " ";
+				}
+				else {
+					returnTime+= command[i-1] + command[i] + " ";
+				}
+			}
+			else {
+				fullCommand+= command[i] + " ";
+			}
+		}
+		String stringNumbers = stringToMath.convert(fullCommand);
+		String calcNumbers = calc.calculate(stringNumbers);
+		int numbers = Integer.parseInt(calcNumbers);
+		stringToMath.clear();
+		timer = new Timer();
+		timer.schedule(new reminder(), numbers * 1000);
+
+		return "Set a timer for " + returnTime;
+
+	}
+	class reminder extends TimerTask {
+		public void run() {
+			timerTextToSpeech.setVoice("dfki-poppy-hsmm");
+			timerTextToSpeech.speak("BEEEEEEEEEEEEEEEEEEEEEEEEEP BEEEP BEEEEP BEEEEEP BEEP", 1.5f, false, true);
+			timer.cancel();
+		}
+	}
+}
