468465d75f791bcbb5e1a2d5ed23f220902c88bc
ijg1996 <92048967+ijg1996@users.noreply.github.com>
2021-12-06 14:02:10 -0500

    GUI changes
    More bee like

--- BumblebeeJava/src/GUI.java
+++ BumblebeeJava/src/GUI.java
@@ -14,6 +14,15 @@ import java.io.FileReader;
 import java.util.Scanner;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.GridLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.SpringLayout;
 
 public class GUI {
 
@@ -88,23 +97,40 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.ORANGE);
+		frame.getContentPane().setBackground(new Color(240, 230, 140));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
 		
 		JLabel lblNewLabel = new JLabel("Input:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 3, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 10, SpringLayout.WEST, frame.getContentPane());
 		frame.getContentPane().add(lblNewLabel);
 		
 		inputField = new JTextField();
+		inputField.setForeground(new Color(255, 255, 255));
+		inputField.setBackground(new Color(0, 0, 0));
+		springLayout.putConstraint(SpringLayout.NORTH, inputField, 0, SpringLayout.NORTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.WEST, inputField, 6, SpringLayout.EAST, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.SOUTH, inputField, 77, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, inputField, -10, SpringLayout.EAST, frame.getContentPane());
 		inputField.setEditable(false);
 		frame.getContentPane().add(inputField);
 		inputField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Output:");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 112, SpringLayout.SOUTH, lblNewLabel);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_1, 0, SpringLayout.EAST, lblNewLabel);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		outputField = new JTextField();
+		outputField.setForeground(new Color(255, 255, 255));
+		outputField.setBackground(new Color(0, 0, 0));
+		springLayout.putConstraint(SpringLayout.NORTH, outputField, 0, SpringLayout.NORTH, lblNewLabel_1);
+		springLayout.putConstraint(SpringLayout.WEST, outputField, 0, SpringLayout.WEST, inputField);
+		springLayout.putConstraint(SpringLayout.SOUTH, outputField, 125, SpringLayout.SOUTH, inputField);
+		springLayout.putConstraint(SpringLayout.EAST, outputField, -17, SpringLayout.EAST, frame.getContentPane());
 		outputField.setEditable(false);
 		frame.getContentPane().add(outputField);
 		outputField.setColumns(10);
--- BumblebeeJava/src/time.java
+++ BumblebeeJava/src/time.java
@@ -1,6 +1,7 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.TimeZone;
 
